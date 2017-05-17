2fa447760f4ad312530293d50873e91ee16078ef
Toby Huang <ruofei77@gmail.com>
2017-05-17 17:25:19 +1000

    arm motor test

--- test_files/test_arm.py
+++ test_files/test_arm.py
@@ -1,8 +1,10 @@
 from ev3dev.ev3 import *
+from time import sleep
 
 
 turnMotor = MiddleMotor(OUTPUT_D)
 
 turnMotor.run_forever(speed_sp=300)
 turnMotor.stop()
+sleep(1)
 turnMotor.run_forever(speed_sp= 0)
