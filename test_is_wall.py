837ac386a521e8de5edd4f45af4e15e10d43f9b0
Toby Huang <ruofei77@gmail.com>
2017-05-24 02:58:45 +1000

    is wall test

---
+++ test_is_wall.py
@@ -0,0 +1,13 @@
+from cor_Aki import *
+# initial the angle of sensor
+usg=us_group()
+usg.turn(90)
+print("finished initialisation")
+while  not btn.any():
+    walls = usg.is_wall()
+
+    print("front", walls[0],"left ",walls[1],"right", walls[2])
+    print("usL =",usg.usL.value(),"usR =", usg.usR.value()*10, "sum =",usg.usL.value()+usg.usR.value()*10)
+    sleep(2)
+
+print("end of the programme")
