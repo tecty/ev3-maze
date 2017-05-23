e6405c50eeb24eacd6f7cd06d03f63b45a721a9b
Toby Huang <ruofei77@gmail.com>
2017-05-24 03:52:57 +1000

    debug cor_aki

--- cor_Aki.py
+++ cor_Aki.py
@@ -35,7 +35,7 @@ tree = Tree()
 """config part these values should be modify """
 # length of one unit
 # unit_length=830 # for clancy in normal speed
-unit_length=1200 # for clancy in normal speed
+unit_length=1050 # for clancy in normal speed
 # unit_length=870 # for E4
 # the color of the can 5 is red
 can_color =5
@@ -165,7 +165,7 @@ def modify_angle():
             direction = -1
 
         # start modify the angle
-        motor_move(direction * 30,-direction * 30)
+        motor_move(-direction * 30,direction * 30)
         while abs(gs.value()) < abs(usg.angle):
             sleep(0.005)
         # end of this modify
