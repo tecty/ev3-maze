a75b673163fe43af0604f8cbe574d5ad59f6f278
Toby Huang <ruofei77@gmail.com>
2017-05-23 20:46:07 +1000

    add a unit test of modify dir

---
+++ test_modify.py
@@ -0,0 +1,11 @@
+from cor_Aki import *
+# initial the angle of sensor
+usg=us_group()
+usg.turn(90)
+print("finished initialisation")
+while  not btn.any():
+    print("usL =",usg.usL.value(),"usR =", usg.usR.value()*10, "sum =",usg.usL.value()+usg.usR.value()*10)
+    print("modify has value ", usg.modify_dir)
+    sleep(2)
+
+print("end of the programme")
