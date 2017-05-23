8c486f65957b4d558397f05433077a5b0f0021e0
Toby Huang <ruofei77@gmail.com>
2017-05-23 20:50:11 +1000

    modify modify dir

--- test_modify.py
+++ test_modify.py
@@ -5,7 +5,7 @@ usg.turn(90)
 print("finished initialisation")
 while  not btn.any():
     print("usL =",usg.usL.value(),"usR =", usg.usR.value()*10, "sum =",usg.usL.value()+usg.usR.value()*10)
-    print("modify has value ", usg.modify_dir)
+    print("modify has value ", usg.modify_dir())
     sleep(2)
 
 print("end of the programme")
