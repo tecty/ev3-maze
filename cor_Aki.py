ea5c89ae1294192e975943dd777d3cf6c5ae7166
Toby Huang <ruofei77@gmail.com>
2017-05-24 22:15:34 +1000

    modify back to

--- cor_Aki.py
+++ cor_Aki.py
@@ -255,7 +255,7 @@ if __name__ == '__main__':
     # revise to the last node
     motor_move(-200,-200)
     print("old position is",rightMotor.position,"before_distance is",before_distance)
-    while (not btn.any()) and rightMotor.position<before_distance:
+    while (not btn.any()) and rightMotor.position>before_distance:
         sleep(0.01)
     motor_stop()
 
